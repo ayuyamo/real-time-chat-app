7e6a033fada35222e1a45372b07accf145b33723
ayuyamo <doq300861@gmail.com>
2025-03-20 11:06:44 -0700

    delete storage rules

--- src/app/lib/firebaseConfig.ts
+++ src/app/lib/firebaseConfig.ts
@@ -3,14 +3,15 @@ import { getAuth } from 'firebase/auth';
 import { getAnalytics } from 'firebase/analytics';
 import { getFirestore } from 'firebase/firestore';
 
-const firebaseConfig = { // web app's Firebase configuration
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+const firebaseConfig = {
+  // web app's Firebase configuration
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig); // initialize Firebase
@@ -18,8 +19,9 @@ const auth = getAuth(app); // get the Firebase Auth service
 const db = getFirestore(app); // get the Firestore service
 
 let analytics;
-if (typeof window !== 'undefined') { // check if window is defined
-    analytics = getAnalytics(app); // get the Firebase Analytics service
+if (typeof window !== 'undefined') {
+  // check if window is defined
+  analytics = getAnalytics(app); // get the Firebase Analytics service
 }
 
 export { auth, analytics, db };
