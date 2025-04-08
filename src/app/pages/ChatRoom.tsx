191d55a23a2cd4bc9824f8b1370aa2abad7571e2
ayuyamo <doq300861@gmail.com>
2025-04-07 18:24:02 -0700

    adjust scale of input box and send & sign out button

--- src/app/pages/ChatRoom.tsx
+++ src/app/pages/ChatRoom.tsx
@@ -23,29 +23,27 @@ const ChatRoom: React.FC<ChatRoomProps> = ({ user }) => {
         <div>
             <h1>Welcome to the chat room, {user.displayName}</h1>
             <RealTimeMessages user={user} /> {/* display messages in real time */}
-            <form onSubmit={handleSubmit}> {/* form for sending messages */}
-                <input
-                    className="message-input text-black"
-                    value={formValue} // bind the input value to the formValue state
-                    onChange={(e) => setFormValue(e.target.value)} // update formValue when the input changes
-                    placeholder="Type a message"
-                />
-                <div className="button-container">
-                    <button 
-                        type="submit"
-                        className="button button-send"
-                    >
+            <form onSubmit={handleSubmit} className='flex p-4 gap-4 justify-between items-center'> {/* form for sending messages */}
+                <div className="relative flex-1">
+                    <input className="block w-full p-4 ps-10 text-sm focus:ring-0 focus:outline-none text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
+                        value={formValue}
+                        onChange={(e) => setFormValue(e.target.value)} // update formValue when the input changes
+                        placeholder="Type Message..." required />
+                    <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Send
                     </button>
+                </div>
+                <div>
                     <button
                         onClick={signOutUser}
-                        className="button button-signout"
+                        className="button button-signout text-small"
                     >
                         Sign out
                     </button>
                 </div>
-            </form>
-        </div>
+                {/* </div> */}
+            </form >
+        </div >
     );
 }
 
