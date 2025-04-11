d15a2a21e6e5849069150ec00d023b73269fd2a5
ayuyamo <doq300861@gmail.com>
2025-04-11 08:37:01 -0700

    fix unexpected pop up when user signed out without typing in message box

--- src/app/pages/ChatRoom.tsx
+++ src/app/pages/ChatRoom.tsx
@@ -71,7 +71,7 @@ const ChatRoom: React.FC<ChatRoomProps> = ({ user }) => {
                     <input className="block w-full p-4 ps-10 text-sm focus:ring-0 focus:outline-none text-gray-900 border border-gray-300 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
                         value={formValue}
                         onChange={handleTyping} // update formValue when the input changes
-                        placeholder="Type Message..." required />
+                        placeholder="Type Message..." />
                     <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                         Send
                     </button>
