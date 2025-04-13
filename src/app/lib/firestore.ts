cd2a845486dc13381b90c4f32a5b4a470abf8417
Aditya Bhagat <141161589+Adiiii03@users.noreply.github.com>
2025-04-12 19:47:15 -0700

    Update firestore.ts

--- src/app/lib/firestore.ts
+++ src/app/lib/firestore.ts
@@ -1,22 +1,48 @@
-import { collection, addDoc } from "firebase/firestore";
+// import { collection, addDoc } from "firebase/firestore";
+// import { db } from "./firebaseConfig";
+
+// export const addMessage = async ( // add a new message to the Firestore database
+//     message: string, 
+//     uid: string,
+//     photoURL: string,
+//     displayName: string
+// ) => {
+//     try { // add a new document with a generated ID
+//         const docRef = await addDoc(collection(db, "messages"), {
+//             message: message,
+//             uid: uid,
+//             photoURL: photoURL,
+//             displayName: displayName,
+//             createdAt: new Date(),
+//         });
+//         console.log("Document written with ID: ", docRef.id);
+//     } catch (error) {
+//         console.error("Error adding document: ", error);
+//     }
+// }
+
+//updated
+import { collection, addDoc, serverTimestamp } from "firebase/firestore";
 import { db } from "./firebaseConfig";
 
-export const addMessage = async ( // add a new message to the Firestore database
-    message: string, 
-    uid: string,
-    photoURL: string,
-    displayName: string
+export const addMessage = async (
+  message: string,
+  uid: string,
+  photoURL: string,
+  displayName: string,
+  imageUrl = ""
 ) => {
-    try { // add a new document with a generated ID
-        const docRef = await addDoc(collection(db, "messages"), {
-            message: message,
-            uid: uid,
-            photoURL: photoURL,
-            displayName: displayName,
-            createdAt: new Date(),
-        });
-        console.log("Document written with ID: ", docRef.id);
-    } catch (error) {
-        console.error("Error adding document: ", error);
-    }
-}
+  try {
+    const docRef = await addDoc(collection(db, "messages"), {
+      message,
+      uid,
+      photoURL,
+      displayName,
+      imageUrl,
+      createdAt: serverTimestamp(), //the correct Firestore timestamp
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (error) {
+    console.error("Error adding document: ", error);
+  }
+};
